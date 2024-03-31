25c87bf9de41b1a6e8bd8a35250f952853657456
Ergys Hila <ergysmania@gmail.com>
2024-03-31 12:41:51 +0300

    Initial commit

---
+++ src/test/java/com/ergys/vesseltracker/VesselTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ergys.vesseltracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VesselTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
